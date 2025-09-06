52200bae1c3340d78403d27cbc1ee30454cba3e6
GarrisonMoore <mooreg444@gmail.com>
2025-09-05 19:00:57 -0700

    please err0r

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("please error");
         System.out.println("error");
+        double y = 323.2223;
     }
 }
